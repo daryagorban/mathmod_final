b2f0beb15324d244a76ce80acda9737ecef7cac0
daryagorban <98222302+daryagorban@users.noreply.github.com>
2025-06-05 23:00:04 +0300

    Add files via upload

---
+++ tleLib/core/stdafx.h
@@ -0,0 +1,21 @@
+//
+// stdafx.h
+//
+// Copyright (c) 2003-2013 Michael F. Henry
+//
+#pragma once
+
+#include <stdio.h>
+
+#ifdef WIN32
+    #include <tchar.h>
+#endif
+
+#include <assert.h>
+#include <math.h>
+#include <time.h>
+
+#include <string>
+#include <map>
+
+using namespace std;
